7e395c46c2ddd7d293a9fe0fdfb075956dcc30eb
Stefan Fleck <stefan.b.fleck@gmail.com>
2018-09-16 13:27:58 +0200

    added indent analysis

--- R/analyse.R
+++ R/analyse.R
@@ -1,18 +1,28 @@
 analyse_package <- function(
   dir = "."
 ){
-
   withr::with_dir(dir, {
     log <- get_log(dir)
-    src <- log[is_r_file(entity)]$entity
-    loc <- calc_loc(unique(src[file.exists(src)]))
+
+    src_files <- log[is_r_file(entity)]$entity
+    src_files <- unique(src_files[file.exists(src_files)])
+
+    loc <- analyse_loc(unique(src_files))
     loc <- data.table(
       entity = names(loc),
       loc = unname(loc)
     )
+
+    ind <- analyse_indent(src_files)
+    ind <- data.table(
+      entity = names(ind),
+      indent = unname(ind)
+    )
   })
 
-  res <- merge(log, loc, by = "entity", all = TRUE)
+  res <- merge(log, loc, by = "entity", all.x = TRUE)
+  res <- merge(res, ind, by = "entity", all.x = TRUE)
+  res
 }
 
 
@@ -21,3 +31,5 @@ analyse_package <- function(
 is_r_file <- function(x){
   grepl("(.*R$)|(.*r$)", x)
 }
+
+
